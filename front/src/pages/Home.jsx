eed8a9986e20bdf31d84ed6a24e4231a03a735d5
2leesanghee2 <leesanghee0317@gmail.com>
2026-05-26 22:49:42 +0900

    🐛 Fix: 홈에서 아이돌 선택 모달 자동 노출 제거 (/select 라우트로 분리)

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -86,7 +86,7 @@ export default function Home({ selectedIdol, onIdolChange, started, skipIdolProm
 
   const { filteredPlaces } = usePlaces(selectedIdol?.id)
   const { removeCourse } = useCourse()
-  const shouldOpenIdolModal = isModalOpen || (started && !selectedIdol && !skipIdolPrompt)
+  const shouldOpenIdolModal = isModalOpen
 
   const handleIdolSelect = (idol) => {
     onIdolChange(idol)
